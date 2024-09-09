9b17ecd4a88c840f7ad1856613c04f7268d0e99d
tsnthiago <thiago_tsn@live.com>
2024-09-09 19:55:25 -0300

    ...

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,7 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-// Declaração do tipo da variável global
+// Declaração do tipo da variável global para o MongoClient Promise
 declare global {
+  // Permite que o uso de _mongoClientPromise em Node.js não cause problemas de redefinição de tipo
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
@@ -13,15 +14,16 @@ const uri = process.env.MONGODB_URI;
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+// Lógica de ambiente de desenvolvimento e produção
 if (process.env.NODE_ENV === 'development') {
-  // Em desenvolvimento, use uma conexão global para reutilizar entre reloads
+  // Em desenvolvimento, reutilizamos a conexão durante os recarregamentos
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // Em produção, crie uma nova conexão a cada request
+  // Em produção, criamos uma nova conexão a cada instância
   client = new MongoClient(uri);
   clientPromise = client.connect();
 }
